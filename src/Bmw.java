3493ee7b3c15886208e1875d35552306a9b019d8
maksimus <stepanov_882@mail.ru>
2022-10-26 16:20:44 +0300

    Initial commit

---
+++ src/Bmw.java
@@ -0,0 +1,10 @@
+public class Bmw {
+    String name1;
+    String infoM8;
+    String infoX5;
+    String infoI8;
+    int priceM8;
+    int priceX5;
+    int priceI8;
+
+}
---
+++ src/Mercedes.java
@@ -0,0 +1,11 @@
+public class Mercedes {
+    String name;
+    String infoG200;
+    String infoGLE250;
+    String infoGLS350;
+    String infoS320;
+    int priceG200;
+    int priceGLE250;
+    int priceGLS350;
+    int priceS320;
+}
